68d66858bddef10b901e6858f5e18e2133eea588
DinguzTheDev <128646877+DinguzTheDev@users.noreply.github.com>
2026-09-25 08:22:14 +0200

    Update lunarloadstring.lua

--- lunarloadstring.lua
+++ lunarloadstring.lua
@@ -1,1 +1,8 @@
+-- automatization made by dinguz / scripts made by Lunar Scripts <3
 
+if game.PlaceId == 107778070777162 then
+	loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/04535ff683cb4b18aa78b2c1e2be1c3e7de2c0c766a76a145b8888c7664e9b62/download"))()
+elseif game.PlaceId == 124216119978534 then
+	loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/69d6dab8b4de36fbd70f01415c3ca172d14c9450c707da149afadbc4de07f7f3/download"))()
+else
+  print("Game not supported! | Lunar Scripts")
